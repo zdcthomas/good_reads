4ae1f61dae7dc25f955fddbbb9e3cb392189a86e
zdcthomas <zdcthomas@yahoo.com>
2018-05-18 10:36:26 -0600

    TESTS user story feature tests

--- spec/features/book_show_pages_spec.rb
+++ spec/features/book_show_pages_spec.rb
@@ -4,30 +4,87 @@ require 'rails_helper'
 describe 'a user' do
   descrbie ' visits the book show page' do 
     it 'should display the book title' do
+      book = Book.create!(title:"American Gods")
+
+      visit book_path book
+
       expect(page).to have_content(title)
     end
+    it 'should show the average rating for that book' do
+      book = Book.create!(title:"American Gods")
+      user = User.create!(name:"Emma")
+      user2 = User.create!(name:"Mike")
+      text1 = "I hate good writing"
+      text2 = "I liked it"
+      user.reviews.create!(score:2, text: text1, book_id: book.id)
+      user2.reviews.create!(score:4, text: text2, book_id: book.id)
+
+      visit book_path book
+
+      expect(page).to have_content("Average Rating: 3")
+    end
+
     it 'should display a list of reviews for that book' do
-      expect(page).to have_content(text)
-      expect(page).to have_content(user_name)
+      book = Book.create!(title:"American Gods")
+      user = User.create!(name:"Emma")
+      user2 = User.create!(name:"Mike")
+      text1 = "I hate good writing"
+      text2 = "I liked it"
+      score1 = 2
+      score2 = 4
+      user.reviews.create!(score:score1, text: text1, book_id: book.id)
+      user2.reviews.create!(score:score2, text: text2, book_id: book.id)
+
+      visit book_path book
+
+      expect(page).to have_content(text1)
+      expect(page).to have_content(text2)
+      expect(page).to have_content(user1.name)
+      expect(page).to have_content(user2.name)
+      expect(page).to have_content(score1)
+      expect(page).to have_content(score2)
+
     end
-    it 'should display the rating the user gave the book' do
+
+    it 'should display the highest and lowest rating for that book' do
+      book = Book.create!(title:"American Gods")
+      user = User.create!(name:"Emma")
+      user2 = User.create!(name:"Mike")
+
+      text1 = 'I hate good writing'
+      text2 = 'I liked it'
+      score1 = 2
+      score2 = 4
+
+      user.reviews.create!(score:score1, text: text1, book_id: book.id)
+      user2.reviews.create!(score:score2, text: text2, book_id: book.id)
+
+      visit book_path book
+
+      expect(page).to have_content("Highest Rating:#{score2}")
+      expect(page).to have_content("Lowest Rating:#{score1}")
     end
-    it 'should display the average rating for that book' do
-    end
-    it 'should display the highest rating for that book' do
-      expect(page).to have_content("Highest Rating:#{rating}")
-    end
-    it 'should display the lowest rating for that book' do
-      expect(page).to have_content("Lowest Rating:#{rating}")
-    end
+
     it 'should display the review text for the highest reviewer under the highest review header' do
-      expect(page).to have_content(text)
-      expect(page).to have_content(name)
+      book = Book.create!(title:"American Gods")
+      user = User.create!(name:"Emma")
+      user2 = User.create!(name:"Mike")
+      text1 = "I hate good writing"
+      text2 = "I liked it"
+      score1 = 2
+      score2 = 4
+      user.reviews.create!(score:score1, text: text1, book_id: book.id)
+      user.reviews.create!(score:score2, text: text2, book_id: book.id)
+      
+      visit book_path book
+      within(".highest_rating") do
+        expect(page).to have_content(text2)
+      end
+      within(".lowest_rating") do
+        expect(page).to have_content(text1)
+      end
     end
-    it 'should display the review text for the lowest reviewer under the lowest review header' do
-      expect(page).to have_content(text)
-      expect(page).to have_content(name)
-    end
+
   end
 end
 
