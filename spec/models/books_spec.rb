312178d29dfa8d5d40de1962e92293902af2eb53
zdcthomas <zdcthomas@yahoo.com>
2018-05-18 10:22:32 -0600

    TESTS book and user model

--- spec/models/books_spec.rb
+++ spec/models/books_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it{should have_many(:reviews)}
+  it{should validate(:title)}
 end
